a1039e5ce64d74c429d0dd90a1cd89c528119dff
Paul Ryvkin <paul.ryvkin@10xgenomics.com>
2018-07-19 00:01:26 -0700

    Fix OOMs in SUMMARIZE_COUNTER_REPORTS_PD and RUN_MULTIGENOME_ANALYSIS #resolve CELLRANGER-1420 CELLRANGER-1421 (#910)
    Fix OOMs in SUMMARIZE PD and MULTIGENOME #resolve CELLRANGER-1420 CELLRANGER-1421

--- mro/stages/analyzer/run_multigenome_analysis/__init__.py
+++ mro/stages/analyzer/run_multigenome_analysis/__init__.py
@@ -2,10 +2,10 @@
 #
 # Copyright (c) 2017 10X Genomics, Inc. All rights reserved.
 #
-
+import math
 import cellranger.analysis.multigenome as cr_mg_analysis
 import cellranger.h5_constants as h5_constants
-import cellranger.matrix as cr_matrix
+from cellranger.matrix import CountMatrix
 import cellranger.io as cr_io
 
 __MRO__ = """
@@ -24,19 +24,19 @@ stage RUN_MULTIGENOME_ANALYSIS(
 
 def split(args):
     if args.skip or not args.is_multi_genome:
-        return {'chunks': [{'__mem_gb': h5_constants.MIN_MEM_GB}]}
+        return {'chunks': []}
+
+    mem_gb = CountMatrix.get_mem_gb_from_matrix_h5(args.raw_matrices_h5)
+    mem_gb += CountMatrix.get_mem_gb_from_matrix_h5(args.filtered_matrices_h5)
 
     chunks = [{
-        '__mem_gb': round(1.5 * h5_constants.MIN_MEM_GB)
+        '__mem_gb': int(math.ceil(max(h5_constants.MIN_MEM_GB, mem_gb)))
     }]
     return {'chunks': chunks}
 
 def main(args, outs):
-    if args.skip or not args.is_multi_genome:
-        return
-
-    raw_matrix = cr_matrix.CountMatrix.load_h5_file(args.raw_matrices_h5)
-    filtered_matrix = cr_matrix.CountMatrix.load_h5_file(args.filtered_matrices_h5)
+    raw_matrix = CountMatrix.load_h5_file(args.raw_matrices_h5)
+    filtered_matrix = CountMatrix.load_h5_file(args.filtered_matrices_h5)
     analysis = cr_mg_analysis.MultiGenomeAnalysis(raw_matrix, filtered_matrix)
     analysis.run_all()
     analysis.save_summary_json(outs.multi_genome_summary)
